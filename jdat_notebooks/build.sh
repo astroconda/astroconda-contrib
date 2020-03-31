ced56d8013361fcf761944b1ee2df4eb223f4b67
Joseph Hunkeler <jhunkeler@gmail.com>
2020-03-31 11:20:43 -0400

    Minor improvements:
    * Increase verbosity
    * Exclude .git* related files in destination
    * Include environment.yml in destination

--- jdat_notebooks/build.sh
+++ jdat_notebooks/build.sh
@@ -11,4 +11,4 @@ if [[ -d ${usedir}/work ]]; then
 fi
 
 mkdir -p "${dest}"
-rsync -a "${usedir}"/notebooks/ "${dest}"/
+rsync -av --exclude '.git*' "${usedir}"/environment.yml "${usedir}"/notebooks/ "${dest}"/
